0090f15cf1de12368e5e280940854e0795379fe3
damodarkhedkar <36823228+damodarkhedkar@users.noreply.github.com>
2020-02-13 10:23:29 +0530

    firstsave

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace CallByValue
+    {
+        class Program
+        {
+            
+            public void Show(int val)
+            {
+                val *= val; 
+                Console.WriteLine("Value inside the show function " + val);
+                
+            }
+            
+            static void Main(string[] args)
+            {
+                int val = 50;
+                Program program = new Program();  
+                Console.WriteLine("Value before calling the function " + val);
+                program.Show(val);           
+                Console.WriteLine("Value after calling the function " + val);
+            }
+        }
+    }
+
